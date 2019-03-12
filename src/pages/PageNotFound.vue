74e34c414953c9b843ff3eb03a7974f0296671bd
Paul Bovis <paul.bovis@canada.ca>
2019-03-12 15:59:54 -0600

    Create `PageNotFound` component

--- src/pages/PageNotFound.vue
+++ src/pages/PageNotFound.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="col-ful">
-    <h1>Not found</h1>
+  <div class="col-ful" :class="$style.centered">
+    <h1 :class="$style.headingLarge">Not found</h1>
 
-    <p>
+    <p :class="$style.textLarge">
       Oooops. We couldn't find what you were loking for. Why don't you 
       <router-link :to="{name: 'Home'}">go home instead</router-link>?
     </p>
@@ -13,12 +13,15 @@
 export default {}
 </script>
 
-<style scoped>
-  h1 {
+<style module>
+  .centered {
+    text-align: center;
+  }
+  .headingLarge {
     font-size: 100px;
   }
-  p {
+  .textLarge {
     font-size: 50px;
-    font-weight: 300;
+    font-weight: 100;
   }
 </style>
